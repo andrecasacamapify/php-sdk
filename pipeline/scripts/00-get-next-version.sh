21ae164f17d5a4263ee49b6c4800dd6e9b887d18
Andre Casaca <andre.casaca@focus-bc.com>
2019-04-12 14:54:14 +0100

    prevent empty versions

--- pipeline/scripts/00-get-next-version.sh
+++ pipeline/scripts/00-get-next-version.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+EXISTING_VERSION_TAG=$(git describe --exact-match --tags HEAD || echo "false")
+if [ "$EXISTING_VERSION_TAG" != false ]; then
+    echo $EXISTING_VERSION_TAG
+    exit
+fi
+
 # get highest tag number
-EXISTING_VERSION_TAG=$(git describe --abbrev=0 --tags || echo "0.0.0")
-EXISTING_VERSION=${EXISTING_VERSION_TAG:-1}
+LAST_VERSION_TAG=$(git describe --abbrev=0 --tags || echo "0.0.0")
+LAST_VERSION=${LAST_VERSION_TAG:-1}
 
 # get number parts and increase last one by 1
-EXISTING_VERSION_MAJOR=$(echo $EXISTING_VERSION | cut -d'.' -f1 | sed -e "s/v//g")
-EXISTING_VERSION_MINOR=$(echo $EXISTING_VERSION | cut -d'.' -f2)
-EXISTING_VERSION_PATCH=$(echo $EXISTING_VERSION | cut -d'.' -f3)
+LAST_VERSION_MAJOR=$(echo $LAST_VERSION_TAG | cut -d'.' -f1 | sed -e "s/v//g")
+LAST_VERSION_MINOR=$(echo $LAST_VERSION_TAG | cut -d'.' -f2)
+LAST_VERSION_PATCH=$(echo $LAST_VERSION_TAG | cut -d'.' -f3)
 
 # get major and minor version numbers
 PACKAGE_VERSION=$1
@@ -17,31 +23,31 @@ PACKAGE_VERSION_MAJOR=$(echo $PACKAGE_VERSION | cut -d'.' -f1)
 PACKAGE_VERSION_MINOR=$(echo $PACKAGE_VERSION | cut -d'.' -f2)
 
 # determine new version
-NEW_VERSION_MAJOR=$EXISTING_VERSION_MAJOR
-NEW_VERSION_MINOR=$EXISTING_VERSION_MINOR
+NEW_VERSION_MAJOR=$LAST_VERSION_MAJOR
+NEW_VERSION_MINOR=$LAST_VERSION_MINOR
 NEW_VERSION_PATCH=0
 
 SKIP_TAGGING=false
 
-if [ -z "$EXISTING_VERSION_TAG" ]; then # use package version numbers if no previous tags exist
+if [ -z "$LAST_VERSION_TAG" ]; then # use package version numbers if no previous tags exist
     NEW_VERSION_MAJOR=$PACKAGE_VERSION_MAJOR
     NEW_VERSION_MINOR=$PACKAGE_VERSION_MINOR
 
-elif [ "$PACKAGE_VERSION_MAJOR" -lt "$EXISTING_VERSION_MAJOR" ]; then
+elif [ "$PACKAGE_VERSION_MAJOR" -lt "$LAST_VERSION_MAJOR" ]; then
     SKIP_TAGGING=true # can't decrement version - skip tagging
 
-elif [ "$PACKAGE_VERSION_MAJOR" -gt "$EXISTING_VERSION_MAJOR" ]; then
+elif [ "$PACKAGE_VERSION_MAJOR" -gt "$LAST_VERSION_MAJOR" ]; then
     NEW_VERSION_MAJOR=$PACKAGE_VERSION_MAJOR
     NEW_VERSION_MINOR=$PACKAGE_VERSION_MINOR
 
-elif [ "$PACKAGE_VERSION_MINOR" -lt "$EXISTING_VERSION_MINOR" ]; then
+elif [ "$PACKAGE_VERSION_MINOR" -lt "$LAST_VERSION_MINOR" ]; then
     SKIP_TAGGING=true # can't decrement version - skip tagging
 
-elif [ "$PACKAGE_VERSION_MINOR" -gt "$EXISTING_VERSION_MINOR" ]; then
+elif [ "$PACKAGE_VERSION_MINOR" -gt "$LAST_VERSION_MINOR" ]; then
     NEW_VERSION_MINOR=$PACKAGE_VERSION_MINOR
 
 else
-    NEW_VERSION_PATCH=$((EXISTING_VERSION_PATCH+1))
+    NEW_VERSION_PATCH=$((LAST_VERSION_PATCH+1))
 fi
 
 # skip tagging if a versioning mismatch has occurred
