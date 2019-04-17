aba17fc15fb672b57a4559f6e082155090a6f49c
Andre Casaca <andre.casaca@focus-bc.com>
2019-04-17 14:13:07 +0100

    Pipeline unnecessary test volumes removed

--- pipeline/scripts/02-test.sh
+++ pipeline/scripts/02-test.sh
@@ -14,6 +14,6 @@ pushd "$script_dir" > /dev/null
 cd ../..
 
 echo "Testing PHP$php_version"
-docker run -e TEST_VALID_API_KEY="$valid_api_key" -e TEST_PUBLIC_KEY_BASE64="$public_key" -e TEST_BASE_URI="$base_uri" -v $(pwd)/tests/results:/sdk/tests/results "mapify-sdk-test:$php_version.$version" -c php composer.phar run test
+docker run -e TEST_VALID_API_KEY="$valid_api_key" -e TEST_PUBLIC_KEY_BASE64="$public_key" -e TEST_BASE_URI="$base_uri" "mapify-sdk-test:$php_version.$version" -c php composer.phar run test
 
 popd > /dev/null
