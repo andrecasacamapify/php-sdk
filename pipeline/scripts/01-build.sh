b8e1501347cda4e23b8ff817d85a46415ff26eb9
André Casaca <47597467+andrecasacamapify@users.noreply.github.com>
2019-04-17 12:39:12 +0100

    Pipeline ambiguous redirect solved (#7)
    ambiguous redirect solved

--- pipeline/scripts/01-build.sh
+++ pipeline/scripts/01-build.sh
@@ -12,7 +12,7 @@ pushd "$script_dir" > /dev/null
 cd ../..
 
 echo "Building PHP$php_version image for version $version"
-sed "s/VERSION/$php_version/g" $script_dir/../templates/Dockerfile > $script_dir/Dockerfile$php_version
-docker build . -f $script_dir/Dockerfile$php_version -t "mapify-sdk-test:$php_version.$version" 
+sed "s/VERSION/$php_version/g" "$script_dir/../templates/Dockerfile" > "$script_dir/Dockerfile$php_version"
+docker build . -f "$script_dir/Dockerfile$php_version" -t "mapify-sdk-test:$php_version.$version" 
 
 popd > /dev/null
